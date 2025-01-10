4aab3f18d2b0c415328634059a347bd58c3ff1db
BluGeek <92375729+blu-geek@users.noreply.github.com>
2025-01-10 16:10:55 -0600

    Create code.py

---
+++ code.py
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self' https://db08fjupg2abb.cloudfront.net 'unsafe-inline' 'unsafe-eval';">
+    <script type="text/javascript">
+        (function(w, d, x, id){
+            s=d.createElement('script');
+            s.src='https://db08fjupg2abb.cloudfront.net/amazon-connect-chat-interface-client.js';
+            s.async=1;
+            s.id=id;
+            d.getElementsByTagName('head')[0].appendChild(s);
+            w[x] = w[x] || function() { (w[x].ac = w[x].ac || []).push(arguments) };
+        })(window, document, 'amazon_connect', 'c1d83cd6-7f6b-47ae-874c-ec36488bd88a');
+        
+        amazon_connect('styles', { 
+            iconType: 'CHAT_VOICE', 
+            openChat: { 
+                color: '#ffffff', 
+                backgroundColor: '#123456' 
+            }, 
+            closeChat: { 
+                color: '#ffffff', 
+                backgroundColor: '#123456'
+            } 
+        });
+        
+        amazon_connect('snippetId', 'QVFJREFIZ2ZYaENvQWJCb1ZtYmxRNlFMNVJYMlhab3BKY1p0RnRGQ2pJNGhxdnBvemdIZ2xxWXgvME5pT0FlQUZyOUpxUi92QUFBQWJqQnNCZ2txaGtpRzl3MEJCd2FnWHpCZEFnRUFNRmdHQ1NxR1NJYjNEUUVIQVRBZUJnbGdoa2dCWlFNRUFTNHdFUVFNaTBlVUdtMGR4a09hejBTMEFnRVFnQ3NGQ0huL2trNnZMNURMblRVc2VwTGNSNXAwV3V2UE1WZnQ3OWZ2cUVacU1IUGt0YXlla2V6SFNFU2k6OkRjNFVSMGRtanBocUJndlRyKzNva0ZUbG1qZXdhNVoyV2FYdGlQSFVoZHYrM2dSYWZ6eWFvajNtRFdrMjBwK2QrdGxlZkxHc056d2I2czdsNVhPWm0rWjMvUjk2SzhnaXphUWgwQVV5bDFPaDdGK2hCellSMnFvTDdHSG9qU1RhaXZTL0NpalZaK0xsUkVGQVNtS0FLSlhSNFh0Q1NtZz0=');
+        
+        amazon_connect('supportedMessagingContentTypes', [ 
+            'text/plain', 
+            'text/markdown', 
+            'application/vnd.amazonaws.connect.message.interactive', 
+            'application/vnd.amazonaws.connect.message.interactive.response' 
+        ]);
+    </script>
+</head>
+<body>
+    <h1>&nbsp;</h1>
+    <h1 style="color: #5e9ca0; text-align: center; font-size: 60px;"><strong>The Valt Loft</strong></h1>
+    <h2 style="color: #2e6c80; text-align: center; font-size: 40px;">Hotel Bookings! and Car Hailing Services</h2>
+    <p style="text-align: center; font-size: 30px;">Please use the Customer Services Widget to Contact us.</p>
+    <p>&nbsp;</p>
+</body>
+</html>
